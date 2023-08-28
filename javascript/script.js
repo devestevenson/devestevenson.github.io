ae7a591e75c2d070f4f334e88b7ce145bb3dfb4c
devestevenson <devestevenson@gmail.com>
2023-08-27 20:11:52 -0500

    devestevenson 2.0

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,5 @@
-//audio de fondo
-function playAudio() {
-  document.querySelector('.audio').play();
-}
 
-
-//menú dezplegable ----------------------
-let dropdown = document.querySelector(".dropdown");
-let dropdownBox = document.querySelector(".dropdownBox");
-
-dropdown.addEventListener("click", function() {
-  if (dropdownBox.style.display === "block") {
-    dropdownBox.style.display = "none";
-  } else {
-    dropdownBox.style.display = "block";
-  }
-});
-
-
-//EFECTO MOUSEMOVE
+// EFECTO MOUSEMOVE
 document.addEventListener("mousemove", cursorMove);
 function cursorMove(e){
   document.querySelectorAll(".move").forEach(function(move){
@@ -40,30 +22,16 @@ gsap.from(".text", .5, {
   delay: 1,
 });
 
-gsap.to(".pre-loader", 2, {
+gsap.to(".pre-loader", 1, {
   top: "-100%",
   ease: "power2.inOut",
   delay: 2,
 });
 
-gsap.from(".homeName", 1, {
+gsap.from(".card-container", .5, {
   y:40,
   opacity: 0,
   ease: "power2.inOut",
-  delay: 4,
-});
-
-gsap.from(".description", 1, {
-  y:40,
-  opacity: 0,
-  ease: "power2.inOut",
-  delay: 5,
-});
-
-gsap.from(".dropdown", 1, {
-  y:40,
-  opacity: 0,
-  ease: "power2.inOut",
-  delay: 6,
+  delay: 3,
 });
 
