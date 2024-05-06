75c0586f80e922b2fa581c3401a0e7a43bdec23a
devestevenson <devestevenson@gmail.com>
2024-05-06 03:59:42 -0500

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		screens: {
+			"minimobile": "374",
+			"mobile": "420px",
+			"tablet": "640px",
+			"tablaptop": "768px",
+			"laptop": "1024px",
+			"desktop": "1280px",
+		},
+		extend: {
+			colors: {
+				mywhite: '#F1F1F1',
+				myblack: '#1F1F1F',
+			},
+		},
+	},
+	plugins: [],
+}
